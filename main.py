189e67eb9b3482f9741d57228c4e2074bc4b455c
Christopher Dignam <chris@dignam.xyz>
2019-08-31 20:03:50 -0400

    .

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+"""
+Modified from https://bostata.com/how-to-populate-fillable-pdfs-with-python/
+"""
+
 import os
+from typing import Optional
+from pathlib import Path
+from time import time
 
 import click
 import pdfrw
@@ -15,21 +22,28 @@ WIDGET_SUBTYPE_KEY = "/Widget"
 @click.option(
     "--input-path", type=click.Path(exists=True, dir_okay=False), required=True
 )
-@click.option("--output-path", type=click.Path(), required=True)
-@click.option("--employee-name", required=True)
-@click.option("--employee-signature", required=True)
-@click.option("--employee-requested-dates", required=True)
-@click.option("--manager-name", required=True)
-@click.option("--manager-signature", required=True)
+@click.option("--output-path", type=click.Path())
+@click.option("--employee-name")
+@click.option("--employee-signature")
+@click.option("--employee-requested-dates")
+@click.option("--manager-name")
+@click.option("--manager-signature")
 def write_fillable_pdf(
-    input_path,
-    output_path,
-    employee_name,
-    employee_signature,
-    employee_requested_dates,
-    manager_name,
-    manager_signature,
+    input_path: str,
+    output_path: Optional[str],
+    employee_name: Optional[str],
+    employee_signature: Optional[str],
+    employee_requested_dates: Optional[str],
+    manager_name: Optional[str],
+    manager_signature: Optional[str],
 ):
+    if output_path is None:
+        path_in = Path(input_path)
+        output_path = str(
+            path_in.with_name(path_in.stem + "-" + str(time())).with_suffix(
+                path_in.suffix
+            )
+        )
     template_pdf = pdfrw.PdfReader(input_path)
     data = dict(
         employee_name=employee_name,
@@ -47,6 +61,7 @@ def write_fillable_pdf(
                     annotation.update(pdfrw.PdfDict(V="{}".format(data[key])))
 
     pdfrw.PdfWriter().write(output_path, template_pdf)
+    click.echo(f"pdf written: {output_path}")
     click.launch(output_path, locate=True)
 
 
